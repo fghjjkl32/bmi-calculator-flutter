9e3536058202d3689e8b435643867ab5a6a4b7bb
fghjjkl32 <fghjjkl32@naver.com>
2021-08-10 07:00:33 +0900

    feat | add : reults page function

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 import '../components/reusable_card.dart';
 import '../components/bottom_button.dart';
-import 'input_page.dart';
 
 class ResultsPage extends StatelessWidget {
+  ResultsPage(
+      {@required this.bmiResult,
+      @required this.resultText,
+      @required this.interpretation});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,15 +42,15 @@ class ResultsPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    "Normal",
+                    resultText.toUpperCase(),
                     style: kResultTextStyle,
                   ),
                   Text(
-                    "18.3",
+                    bmiResult,
                     style: kBMITextStyle,
                   ),
                   Text(
-                    "your BMI result is quite low, you should eat more!",
+                    interpretation,
                     style: kbodyTextStyle,
                     textAlign: TextAlign.center,
                   ),
